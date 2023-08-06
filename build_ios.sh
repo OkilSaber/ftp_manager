7c89c5f420e6485b3308f9d408db0cada01b0ac1
slakhdari <okil.lakhdari@kbrwadventure.com>
2023-08-06 14:36:57 +0200

    Add build script for IOS

---
+++ build_ios.sh
@@ -0,0 +1,10 @@
+flutter build xcarchive --release
+if [ -d "./build/ios/archive/Runner.xcarchive/Products/Applications/Runner.app" ]; then
+    echo "build success"
+    mkdir Payload
+    cp -r ./build/ios/archive/Runner.xcarchive/Products/Applications/Runner.app ./Payload/
+    zip -r ftp-manager.ipa Payload
+    rm -rf Payload
+else
+    echo "build failed"
+fi
